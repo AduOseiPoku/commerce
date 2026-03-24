2b07128dabf16b001af96e13f0a3cc79cbc0caf2
AduPoku <adupokuosei1234@gmail.com>
2026-03-24 20:02:07 +0000

    browse

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-
-from .models import Item
+from django.db.models import Q
+from .models import Category, Item
 from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
@@ -14,8 +14,26 @@ def detail(request, pk):
     })
 
 
-@login_required
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category_id', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
 
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
+
+@login_required
 def new(request):
     if request.method == 'POST':
         form = NewItemForm(request.POST, request.FILES)
